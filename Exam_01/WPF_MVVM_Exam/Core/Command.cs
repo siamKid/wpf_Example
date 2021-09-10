c64743702d9bbe40fc83a422aea18dbc91743621
siamKid <chtaeryu@naver.com>
2021-09-10 14:45:21 +0900

    exam_01

---
+++ Exam_01/WPF_MVVM_Exam/Core/Command.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPF_MVVM_Exam.Core
+{
+    class Command : ICommand
+    {
+        private readonly Action<object> commanExecute;
+        private readonly Func<object, bool> commandCanExcute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public Command(Action<object> excute, Func<object, bool> canExcute = null)
+        {
+            commanExecute = excute;
+            commandCanExcute = canExcute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return commandCanExcute == null || commandCanExcute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            commanExecute(parameter);
+        }
+    }
+}
---
+++ Exam_01/WPF_MVVM_Exam/Core/NotifyPropertyChanged.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF_MVVM_Exam.Core
+{
+    class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
